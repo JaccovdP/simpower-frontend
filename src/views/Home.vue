5d0df0b9b4f683f4cc5c4b0a96bad43961061f10
Jacco van der Plaat <j.c.vanderplaat@students.uu.nl>
2020-07-21 13:59:26 +0200

    - Removed some unused files - Added link to repo on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,15 @@
       Our website is currently a work in progress, so the homepage is not there yet.<br>
       However, the information for our leagues is already available via the navigation bar at the top of the page.<br>
       <br>
-      Please find us on our <b-link href="https://discord.gg/6B67YnY" target="_blank">Discord</b-link>!
+      Please find us on our <b-link href="https://discord.gg/6B67YnY" target="_blank">Discord</b-link>!<br>
+      <br>
+      If you want to contribute to our website, the repository can be found <b-link href="https://github.com/JaccovdP/simpower-frontend" target="_blank">here</b-link>.
     </b-container>
 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
